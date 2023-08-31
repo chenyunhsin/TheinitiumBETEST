336270905595d5f716b646aadf6ecbf1f12f8f17
Yun-Hsin Chen <chen_yun_hsin@amastek.com>
2023-08-31 18:41:46 +0800

    feat: 增加model

--- registration_login/accounts/admin.py
+++ registration_login/accounts/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'is_staff', 'is_employee')
+
+admin.site.register(CustomUser, CustomUserAdmin)
--- registration_login/registration_login/settings.py
+++ registration_login/registration_login/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
     }
 }
 
-
+AUTH_USER_MODEL = 'accounts.CustomUser'
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
